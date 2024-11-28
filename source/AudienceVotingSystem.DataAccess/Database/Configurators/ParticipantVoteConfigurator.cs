be66c567429164279ed0d3ff3f9a1b3ea4815bc4
Dmitriy Vlasyuk <dmitriyvlasyuk@yandex.ru>
2024-11-28 03:13:11 +0300

    Переименование проекта

---
+++ source/AudienceVotingSystem.DataAccess/Database/Configurators/ParticipantVoteConfigurator.cs
@@ -0,0 +1,26 @@
+using System.Diagnostics.CodeAnalysis;
+
+using AudienceVotingSystem.DataAccess.Database.Entities;
+
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace AudienceVotingSystem.DataAccess.Database.Configurators;
+
+/// <summary>
+/// Конфигуратор голоса участника в рамках зрительского голосования.
+/// </summary>
+public class ParticipantVoteConfigurator : IEntityTypeConfiguration<ParticipantVote>
+{
+    /// <summary>
+    /// Конфигурирует модель сущности базы данных.
+    /// </summary>
+    /// <param name="builder">Конструктор для конфигурирования модели.</param>
+    public void Configure([NotNull] EntityTypeBuilder<ParticipantVote> builder)
+    {
+        builder.HasKey(entity => entity.Identifier);
+        builder.Property(entity => entity.ParticipantId).HasMaxLength(50);
+        builder.Property(entity => entity.CandidateId).HasMaxLength(50);
+        builder.Property(entity => entity.Note).HasMaxLength(1000);
+    }
+}
---
+++ source/AudienceVotingSystem.Host/Program.cs
@@ -0,0 +1,53 @@
+using AudienceVotingSystem.DataAccess;
+using AudienceVotingSystem.DataAccess.Database;
+using AudienceVotingSystem.Host.Configuration;
+
+using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+
+using MudBlazor.Services;
+
+namespace AudienceVotingSystem.Host;
+
+/// <summary>
+/// Основной класс приложения, содержащий точку входа.
+/// </summary>
+public static class Program
+{
+    /// <summary>
+    /// Основной метод приложения, являющийся точкой входа.
+    /// </summary>
+    /// <param name="arguments">Аргументы командной строки, переданные приложению.</param>
+    public static void Main(string[] arguments)
+    {
+        var applicationBuilder = WebApplication.CreateBuilder(arguments);
+
+        applicationBuilder.Services.AddRazorPages();
+        applicationBuilder.Services.AddServerSideBlazor();
+        applicationBuilder.Services.AddMudServices();
+
+        applicationBuilder.Services.AddOptions<SecurityConfiguration>().BindConfiguration("SecurityConfiguration");
+        applicationBuilder.Services.AddSingleton<IValidateOptions<SecurityConfiguration>, SecurityConfigurationValidator>();
+
+        applicationBuilder.Services.AddScoped<AuthenticationProvider>();
+        applicationBuilder.Services.AddScoped<AuthenticationStateProvider, AuthenticationProvider>();
+
+        applicationBuilder.Services.AddDataAccess(
+            configurationSection: "ApplicationConfiguration",
+            connectionString: "ApplicationDatabase");
+
+        var application = applicationBuilder.Build();
+
+        using var migrationScope = application.Services.CreateScope();
+        var databaseContext = migrationScope.ServiceProvider.GetRequiredService<DatabaseContext>();
+        databaseContext.Database.Migrate();
+
+        application.UseStaticFiles();
+        application.UseRouting();
+        application.MapBlazorHub();
+        application.MapFallbackToPage("/Application");
+
+        application.Run();
+    }
+}
